a0eaae0dce0a1b61bbb19b468bae9b8812c1a1c9
Satyam-Nayak <satyamn120@gmail.com>
2024-07-17 12:08:06 +0530

    new

---
+++ darshan oops/loop statements/unreachablecode.java
@@ -0,0 +1,13 @@
+public class unreachablecode {
+    public static void main(String[] args) {
+        System.out.println("Start..!");
+        for(int i =1; i <= 10; i++) {
+            break;
+            System.out.println("hello...!");
+        }
+        System.out.println("End..!");
+    }
+}
+
+
+//unreachable code
