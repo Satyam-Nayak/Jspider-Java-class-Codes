f0d612745ffbf0f41875bc49947790b3681b69bf
Satyam-Nayak <satyamn120@gmail.com>
2024-08-05 11:43:29 +0530

    05-08-2024 String Char At( ) darshan sir

---
+++ darshan oops/05-08-2024/q1.java
@@ -0,0 +1,20 @@
+// write a java program traverse the given string
+public class q1 {
+    public static void main(String[] args) {
+         
+        String str = "Hello";
+        
+        for(int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+
+        
+    
+        
+    }
+    
+}
+
+
+
+        // traverse the string character by character
---
+++ darshan oops/05-08-2024/q2.java
@@ -0,0 +1,12 @@
+// write a java program to print the string in reverse order.
+public class q2 {
+    public static void main(String[] args) {
+        // Reverse the string
+        String s = "Hello Satyam";
+        for(int i = s.length()-1; i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+    
+    }
+    
+}
